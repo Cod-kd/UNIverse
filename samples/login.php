77a70a3d824e03fabea4278c6a75dbc9809b14c2
paco7828 <koszegipatrik888@gmail.com>
2024-10-09 12:33:29 +0200

    Created a default implementation of the login process.

---
+++ samples/login.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "universeDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: $conn->connect_error");
+}
+
+$email = htmlspecialchars($_POST["email"]);
+$passwd = htmlspecialchars($_POST["passwd"]);
+
+$stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows === 1) {
+    $row = $result->fetch_assoc();
+
+    if (password_verify($passwd, $row["passwd"])) {
+        echo "Successful login!";
+    } else {
+        echo "Can't log in!";
+    }
+} else {
+    echo "No user found!";
+}
+
+$stmt->close();
+$conn->close();
