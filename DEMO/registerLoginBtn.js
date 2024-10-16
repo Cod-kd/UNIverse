c5a418ca3d3551005423e6a51252ef98beb898a3
paco7828 <koszegipatrik888@gmail.com>
2024-10-16 11:16:12 +0200

    Added and checked quick fix --> new typing effect for h1.

--- DEMO/registerLoginBtn.js
+++ DEMO/registerLoginBtn.js
@@ -1117,4 +1117,10 @@ function extractEmail(text) {
 
 document.addEventListener('DOMContentLoaded', () => {
     arrowBtnsConfig();
+    const typed = new Typed(".auto-type", {
+        strings: ["Az egyetem csak a tanulásról szól?", "NEM!"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
 });
