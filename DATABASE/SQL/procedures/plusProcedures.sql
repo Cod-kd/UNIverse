82037416e09e93bd00d68e24574debb7959a7191
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2024-10-24 12:55:32 +0200

    ADD contact

--- DATABASE/SQL/procedures/plusProcedures.sql
+++ DATABASE/SQL/procedures/plusProcedures.sql
@@ -13,3 +13,19 @@ BEGIN
 	SELECT `name` FROM `categories` WHERE `id` = idIn;
 END$$
 DELIMITER ;
+
+-- Create new contact
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `createContactType`(IN `nameIn` VARCHAR(20), IN `domainIn` VARCHAR(64), IN `protocolIn` VARCHAR(5))
+BEGIN
+	INSERT INTO `contacttypes`(`name`, `domain`, `protocol`) VALUES (nameIn, domainIn, protocolIn);
+END$$
+DELIMITER ;
+
+-- Get contact by id
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getContactType`(IN `idIn` TINYINT)
+BEGIN
+	SELECT `name`, `domain`, `protocol` FROM `contacttypes` WHERE contacttypes.id = idIn;
+END$$
+DELIMITER ;
