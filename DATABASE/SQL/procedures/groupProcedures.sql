6106d219d876cf5574bae586a284f6fa77aab5dc
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2024-10-28 12:33:32 +0100

    ADD group procedures

--- DATABASE/SQL/procedures/groupProcedures.sql
+++ DATABASE/SQL/procedures/groupProcedures.sql
@@ -13,3 +13,61 @@ BEGIN
 	SELECT `name`, `public`, `membersCount`, `postCount`, `actualEventCount`, `allEventCount` FROM `groups` WHERE groups.id = idIn;
 END$$
 DELIMITER ;
+
+
+-- @todo: test this:
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addGroupActualEventCount`(IN `groupIdIn` MEDIUMINT)
+BEGIN
+UPDATE `groups` SET `actualEventCount` = actualEventCount + 1 WHERE groups.id = groupIdIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addGroupAllEventCount`(IN `groupIdIn` MEDIUMINT)
+BEGIN
+UPDATE `groups` SET `allEventCount` = allEventCount + 1 WHERE groups.id = groupIdIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addGroupPostCount`(IN `groupIdIn` MEDIUMINT)
+BEGIN
+UPDATE `groups` SET `postCount` = postCount + 1 WHERE groups.id = groupIdIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addGroupMemberCount`(IN `groupIdIn` MEDIUMINT)
+BEGIN
+UPDATE `groups` SET `membersCount` = membersCount + 1 WHERE groups.id = groupIdIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `reduceGroupActualEventCount`(IN `groupIdIn` MEDIUMINT)
+BEGIN
+UPDATE `groups` SET `actualEventCount` = actualEventCount - 1 WHERE groups.id = groupIdIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `reduceGroupAllEventCount`(IN `groupIdIn` MEDIUMINT)
+BEGIN
+UPDATE `groups` SET `allEventCount` = allEventCount - 1 WHERE groups.id = groupIdIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `reduceGroupMemberCount`(IN `groupIdIn` MEDIUMINT)
+BEGIN
+UPDATE `groups` SET `membersCount` = membersCount - 1 WHERE groups.id = groupIdIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `reduceGroupPostCount`(IN `groupIdIn` MEDIUMINT)
+BEGIN
+UPDATE `groups` SET `postCount` = postCount - 1 WHERE groups.id = groupIdIn;
+END$$
+DELIMITER ;
