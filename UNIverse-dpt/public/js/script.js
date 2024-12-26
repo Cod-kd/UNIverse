ddf607dbc45b2bb94ca544293461fe14f0a65f99
paco7828 <koszegipatrik888@gmail.com>
2024-12-26 16:19:35 +0100

    Added element and variable gets and typing effect to script.js.

--- UNIverse-dpt/public/js/script.js
+++ UNIverse-dpt/public/js/script.js
@@ -1,5 +1,20 @@
+// Elements
+const registerLoginBtn = document.getElementById("registerLoginBtn");
+const registerLoginBtnDiv = document.getElementById("registerLoginBtnDiv");
+const monitorScreenDiv = document.getElementById("monitorScreenDiv");
+
+// Helper variables
+let isEmailChecked = false;
+let lastCheckedEmail = "";
+let isUsernameChecked = false;
+let lastCheckedUsername = "";
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Run upon first load
 document.addEventListener("DOMContentLoaded", () => {
     //arrowBtnsConfig();
+    // Add typing effect to element with 'auto-type' class
     const typed = new Typed(".auto-type", {
         strings: ["Az egyetem csak a tanulásról szól?", "NEM!"],
         typeSpeed: 50,
