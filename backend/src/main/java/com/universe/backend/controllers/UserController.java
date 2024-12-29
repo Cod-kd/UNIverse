af5eeed457722cb475bb65bc4d9afdfcffff88c6
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2024-12-29 17:00:17 +0100

    Update UserController.java

--- backend/src/main/java/com/universe/backend/controllers/UserController.java
+++ backend/src/main/java/com/universe/backend/controllers/UserController.java
@@ -1,12 +1,17 @@
 package com.universe.backend.controllers;
 
+import com.universe.backend.dto.UserRegistrationDTO;
+import com.universe.backend.exceptions.UserAlreadyExistsException;
+import com.universe.backend.services.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.universe.backend.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/user")
@@ -18,5 +23,20 @@ public class UserController {
     public ResponseEntity<Integer> getId(@RequestParam String username) {
         Integer id = us.fetchUserId(username);
         return ResponseEntity.ok(id);
+    }
+    
+    @Autowired
+    private RegistrationService rs;
+            
+    @PostMapping("/registration")
+    public ResponseEntity<String> userRegistration(@RequestBody @Valid UserRegistrationDTO urDTO) {
+        try {
+            rs.registerUser(urDTO);
+            return ResponseEntity.ok("Sikeres regisztráció!");
+        } catch (UserAlreadyExistsException ex) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("A felhasználó már létezik: " + ex.getMessage());
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Váratlan Error regisztráció közben...");
+        }
     }
 }
