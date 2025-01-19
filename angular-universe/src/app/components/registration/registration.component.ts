eba762729efdbabdd36b9f50fb1693a83c02ecb5
paco7828 <koszegipatrik888@gmail.com>
2025-01-19 21:52:38 +0100

    Added university and faculty names to selects at registration.

--- angular-universe/src/app/components/registration/registration.component.ts
+++ angular-universe/src/app/components/registration/registration.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UniversityService } from '../../services/university.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  private fb = inject(FormBuilder);
+  private universityService = inject(UniversityService);
 
+  universities = toSignal(this.universityService.getUniversities());
+  faculties = toSignal(this.universityService.faculties$);
+
+  registrationForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    username: ['', Validators.required],
+    gender: ['', Validators.required],
+    password: ['', Validators.required],
+    birthDate: ['', Validators.required],
+    university: ['', Validators.required],
+    faculty: [{ value: '', disabled: true }, Validators.required]
+  });
+
+  onUniversityChange() {
+    const universityId = this.registrationForm.get('university')?.value;
+    if (universityId) {
+      this.registrationForm.get('faculty')?.enable();
+      this.universityService.loadFaculties(universityId);
+    } else {
+      this.registrationForm.get('faculty')?.disable();
+    }
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+    }
+  }
 }
