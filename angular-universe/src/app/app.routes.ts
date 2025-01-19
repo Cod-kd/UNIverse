8cd03bc2d8b8f58413c73996398842bf02837707
paco7828 <koszegipatrik888@gmail.com>
2025-01-19 19:28:38 +0100

    Added main-site component and its future path.

--- angular-universe/src/app/app.routes.ts
+++ angular-universe/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RegistrationComponent } from './components/registration/registration.co
 import { CenterContainerComponent } from './components/center-container/center-container.component';
 import { LoginComponent } from './components/login/login.component';
 import { UNIcardLoginComponent } from './unicard-login/unicard-login.component';
+import { MainSiteComponent } from './components/main-site/main-site.component';
 
 export const routes: Routes = [
     { path: '', component: CenterContainerComponent },
     { path: 'registration', component: RegistrationComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'UNIcard-login', component: UNIcardLoginComponent }
+    { path: 'UNIcard-login', component: UNIcardLoginComponent },
+    { path: 'main-site', component: MainSiteComponent }
 ];
