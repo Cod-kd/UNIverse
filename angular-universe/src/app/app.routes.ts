7b6e0c84e621d16fe1ae783415940a4a466324ca
paco7828 <koszegipatrik888@gmail.com>
2025-01-19 19:29:42 +0100

    Moved UNIcard-login component to the components folder.

--- angular-universe/src/app/app.routes.ts
+++ angular-universe/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { CenterContainerComponent } from './components/center-container/center-container.component';
 import { LoginComponent } from './components/login/login.component';
-import { UNIcardLoginComponent } from './unicard-login/unicard-login.component';
+import { UNIcardLoginComponent } from './components/unicard-login/unicard-login.component';
 import { MainSiteComponent } from './components/main-site/main-site.component';
 
 export const routes: Routes = [
--- angular-universe/src/app/components/unicard-login/unicard-login.component.ts
+++ angular-universe/src/app/components/unicard-login/unicard-login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from "../components/button/button.component";
+import { ButtonComponent } from "../button/button.component";
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-unicard-login',
