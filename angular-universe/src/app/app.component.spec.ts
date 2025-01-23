375263e3d33f0d894f6954408ec4bb9b1d095926
Kőszegi Patrik <koszegipatrik888@gmail.com>
2025-01-23 15:51:19 +0100

    Removed the expected title attribute in the app.component.specs.ts.

--- angular-universe/src/app/app.component.spec.ts
+++ angular-universe/src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'angular-universe' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-universe');
   });
 
   it('should render title', () => {
