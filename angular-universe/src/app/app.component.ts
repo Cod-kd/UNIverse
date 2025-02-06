425d4b12df201374dd0aebca6c6e12374d2b9d4e
Kőszegi Patrik <koszegipatrik888@gmail.com>
2025-02-06 18:25:19 +0100

    Modified header component to be present on register and login page and added styling.

--- angular-universe/src/app/app.component.ts
+++ angular-universe/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from "./components/main/main.component";
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, MainComponent],
+  imports: [FooterComponent, MainComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
