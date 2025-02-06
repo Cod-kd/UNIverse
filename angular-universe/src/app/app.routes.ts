9856862b62b8ef753c04a55b69c88d2d0a9b026c
Kőszegi Patrik <koszegipatrik888@gmail.com>
2025-02-06 18:39:35 +0100

    Added new routes and configured navigation anchors for redirection, template components created.

--- angular-universe/src/app/app.routes.ts
+++ angular-universe/src/app/app.routes.ts
@@ -5,6 +5,10 @@ import { LoginComponent } from './components/login/login.component';
 import { UNIcardLoginComponent } from './components/unicard-login/unicard-login.component';
 import { MainSiteComponent } from './components/main-site/main-site.component';
 import { UNIcardComponent } from './components/unicard/unicard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { GroupComponent } from './components/group/group.component';
+import { EventComponent } from './components/event/event.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 export const routes: Routes = [
     { path: '', component: CenterContainerComponent },
@@ -12,5 +16,9 @@ export const routes: Routes = [
     { path: 'get-unicard', component: UNIcardComponent },
     { path: 'login', component: LoginComponent },
     { path: 'UNIcard-login', component: UNIcardLoginComponent },
-    { path: 'main-site', component: MainSiteComponent }
+    { path: 'main-site', component: MainSiteComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'groups', component: GroupComponent },
+    { path: 'events', component: EventComponent },
+    { path: 'calendar', component: CalendarComponent }
 ];
