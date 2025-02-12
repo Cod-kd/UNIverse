8bb04f3e9c5ec3be1a4f043c8b0a538390ee4baa
paco7828 <koszegipatrik888@gmail.com>
2025-02-12 10:29:37 +0100

    Modified header styling and added hamburger menu for phone device resolution.

--- angular-universe/src/app/components/header/header.component.ts
+++ angular-universe/src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +8,41 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
+  isMobile = false;
+  private lastScrollPosition = 0;
 
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768;
+    if (!this.isMobile) this.isMenuOpen = false;
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const currentScroll = window.scrollY;
+    if (Math.abs(currentScroll - this.lastScrollPosition) > 10) {
+      this.isMenuOpen = false;
+      this.lastScrollPosition = currentScroll;
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    // Close menu if clicking outside
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isMenuOpen = false;
+    }
+  }
+
+  toggleMenu(event: Event) {
+    event.stopPropagation(); // Prevent document click from immediately closing
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
