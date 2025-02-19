0ad1d3eb4eb1d6ee474d595f86f92cf9ee0647ed
Kőszegi Patrik <koszegipatrik888@gmail.com>
2025-02-19 17:28:03 +0100

    Added animation to welcome main page.

--- angular-universe/src/app/components/main-site-components/main-site/main-site.component.ts
+++ angular-universe/src/app/components/main-site-components/main-site/main-site.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { SearchBarComponent } from '../../general-components/search-bar/search-bar.component';
+import { SvgAnimationComponent } from '../../general-components/svg-animation/svg-animation.component';
 
 @Component({
   selector: 'app-main-site',
   standalone: true,
-  imports: [RouterOutlet, SearchBarComponent],
+  imports: [RouterOutlet, SearchBarComponent, SvgAnimationComponent],
   templateUrl: './main-site.component.html',
   styleUrl: './main-site.component.css'
 })
 export class MainSiteComponent {
   currentUser: string = "user";
+
+  async ngOnInit() {
+    const animationDiv = document.querySelector('#animationDiv');
+    animationDiv?.classList.add('active-animation');
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    animationDiv?.classList.remove('active-animation');
+  }
 
   constructor(private router: Router) {
     const storedData = localStorage.getItem("username");
