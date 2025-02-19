34a790796429aeaa541926da83b49dfe3a984dd1
paco7828 <koszegipatrik888@gmail.com>
2025-02-20 00:16:15 +0100

    Added loading animation for the page.

--- angular-universe/src/app/app.component.ts
+++ angular-universe/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { FooterComponent } from './components/general-components/footer/footer.component';
 import { MainComponent } from "./components/general-components/main/main.component";
 import { HeaderComponent } from './components/general-components/header/header.component';
@@ -8,8 +8,14 @@ import { HeaderComponent } from './components/general-components/header/header.c
   standalone: true,
   imports: [FooterComponent, MainComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements AfterViewInit {
+  isLoading = true;
+
+  ngAfterViewInit() {
+    document.addEventListener('DOMContentLoaded', () => {
+      this.isLoading = false;
+    });
+  }
 }
