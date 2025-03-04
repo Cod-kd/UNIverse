faf543f6a7d70e96d53e53de4163a381988203d3
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2025-03-04 09:17:19 +0100

    Userfactory with hash

--- DATABASE/SQL/factory/main/1/userFactory.sql
+++ DATABASE/SQL/factory/main/1/userFactory.sql
@@ -1,7 +1,7 @@
 -- Generate 5 User entities
-
-CALL registerUser('user1@example.com', 'user1', 'password1', 'John Doe', TRUE, '1990-01-01', 'Computer Science', 'University A', 'jpg');
-CALL registerUser('user2@example.com', 'user2', 'password2', 'Jane Smith', FALSE, '1992-02-02', 'Mathematics', 'University B', 'png');
-CALL registerUser('user3@example.com', 'user3', 'password3', 'Alice Johnson', FALSE, '1995-03-03', 'Biology', 'University C', 'gif');
-CALL registerUser('user4@example.com', 'user4', 'password4', 'Bob Brown', TRUE, '1988-04-04', 'Physics', 'University D', 'jpeg');
-CALL registerUser('user5@example.com', 'user5', 'password5', 'Charlie White', TRUE, '1991-05-05', 'Engineering', 'University E', 'bmp');
+-- password: Password123
+CALL registerUser('user1@example.com', 'user1', '$2y$12$x9Qx33ZDWV3p.eyLSR7zXuUTyUah7/RLlq2apJTQpSEyOn7NXdQz6', 'John Doe', TRUE, '1990-01-01', 'Computer Science', 'University A', 'jpg');
+CALL registerUser('user2@example.com', 'user2', '$2y$12$x9Qx33ZDWV3p.eyLSR7zXuUTyUah7/RLlq2apJTQpSEyOn7NXdQz6', 'Jane Smith', FALSE, '1992-02-02', 'Mathematics', 'University B', 'png');
+CALL registerUser('user3@example.com', 'user3', '$2y$12$x9Qx33ZDWV3p.eyLSR7zXuUTyUah7/RLlq2apJTQpSEyOn7NXdQz6', 'Alice Johnson', FALSE, '1995-03-03', 'Biology', 'University C', 'gif');
+CALL registerUser('user4@example.com', 'user4', '$2y$12$x9Qx33ZDWV3p.eyLSR7zXuUTyUah7/RLlq2apJTQpSEyOn7NXdQz6', 'Bob Brown', TRUE, '1988-04-04', 'Physics', 'University D', 'jpeg');
+CALL registerUser('user5@example.com', 'user5', '$2y$12$x9Qx33ZDWV3p.eyLSR7zXuUTyUah7/RLlq2apJTQpSEyOn7NXdQz6', 'Charlie White', TRUE, '1991-05-05', 'Engineering', 'University E', 'bmp');
