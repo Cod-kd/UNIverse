2baae96d2192f9adbd230919a7263800604f29d2
paco7828 <koszegipatrik888@gmail.com>
2025-03-11 15:09:59 +0100

    Code optimization at center-container component.

--- angular-universe/src/app/components/root-page-components/center-container/center-container.component.spec.ts
+++ angular-universe/src/app/components/root-page-components/center-container/center-container.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CenterContainerComponent } from './center-container.component';
 
 describe('CenterContainerComponent', () => {
